9034cb28834b3a5a046d09d916cf5dadd7c49981
Adrian <adriansalgado215@gmail.com>
2022-07-24 22:08:38 -0500

    modified swagger information

--- src/main/java/com/oz/OzApplication.java
+++ src/main/java/com/oz/OzApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info=@Info(title="Information about earth"))
+@OpenAPIDefinition(info=@Info(title="Humans",
+					contact = @Contact(
+						      name = "Adrian De Cello Salgado",
+						      email = "adriansalgado215@gmail.com")))
 public class OzApplication {
 
 	public static void main(String[] args) {
